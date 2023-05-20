8332177412c85aaaf25bf7879e166cf7c4ff5c3d
Boboy Infante <hm0ejd74@gmail.com>
2023-05-20 09:00:44 +0800

    added comments

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import random
-
+# initialize the moves
 moves = ("rock", "paper", "sccissor")
+# scores
 playerScore = 0
 computerScore = 0
 
+# loops the whole process
 while True:
+    # loops the computer pick
     compick = random.choice(moves)
     
+    # serves as spaces
     print()
     print("[1] Rock")
     print("[2] Paper")
@@ -15,6 +19,7 @@ while True:
     playermove = input("Pick a move: ")
     print('')
     
+    # loops the if statement, and if the move meets the condition it breaks the loop, else it will continue to loop until the condition is meet
     while True:
         if(playermove == '1' or playermove == '2' or playermove == '3'):
             break
@@ -22,7 +27,7 @@ while True:
             print("Invalid Move")
             break
 
-
+            # compares the computer and player move to check if the player won or the computer won, or else tied
     if(playermove == '1' and compick == 'rock'):
         print("TIE")
     elif(playermove == '1' and compick == 'paper'):
@@ -47,11 +52,13 @@ while True:
     elif(playermove == '3' and compick == 'rock'):
         print("Computer Won")
         computerScore+=1
-    print()
+        # serves as spaces
+    print("")
     print('Player: ' + str(playerScore))
     print('Computer: ' + str(computerScore))
     print("")
     
+    # if one of them meets the score of 5 the game ends
     if(playerScore == 5):
         print('Player Won the Game')
         break
